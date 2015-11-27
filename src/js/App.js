d076f86e530e715684f4623021e2c07227fa0185
Nasafato <alanjgou@gmail.com>
2015-11-27 18:41:28 -0500

    Checkboxes are passing their values correctly up the chain

--- src/js/App.js
+++ src/js/App.js
@@ -3,35 +3,79 @@
 //React.render(<Parent />, document.getElementById('app'));
 
 var TagRow = React.createClass({
+    handleChange: function() {
+        this.props.onUserInput(
+            this.props.tagName
+        );
+    },
     render: function() {
-        var tagName = this.props.tagName;
+        var tagName = this.props.tagChecked ?
+            <span style={{color: 'red'}}>
+                {this.props.tagName}
+            </span> :
+            this.props.tagName;
         return (
             <tr>
                 <td>{tagName}</td>
+                <td>
+                    <form>
+                        <input 
+                            type="checkbox" 
+                            checked={this.props.tagChecked} 
+                            onChange={this.handleChange} 
+                        />
+                    </form>
+                </td>
             </tr>
         );
     }
 });
 
+function isTagNameChecked(listOfTagNames, tagName) {
+    for (var i = 0; i < listOfTagNames.length; i++) {
+        if (listOfTagNames[i] === tagName) {
+            return true;
+        }
+    }
+    return false;
+}
 
 var TagTable = React.createClass({
     render: function() {
+        var self = this;
         var rows = [];
+        var tagsChecked = this.props.tagsChecked;
         this.props.tagNames.forEach(function(tagName) {
-            rows.push(<TagRow tagName={tagName} />);
-        });
+            if (isTagNameChecked(tagsChecked, tagName)) {
+                rows.push(<TagRow
+                            tagName={tagName}
+                            tagChecked={true}
+                            onUserInput={self.props.onUserInput}
+                            />);
+            } else {
+                rows.push(<TagRow 
+                            tagName={tagName} 
+                            tagChecked={false}
+                            onUserInput={self.props.onUserInput}
+                            />);
+            }
+        }.bind(this));
 
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Tag Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
+            <div className="tagTable row">
+                <div className="col-lg-12">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Tag Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 });
@@ -39,31 +83,55 @@ var TagTable = React.createClass({
 var SlideView = React.createClass({
     render: function() {
         return (
-            <div className="slideView">
-                <img src={this.props.imageURL} />
+            <div className="slideView row">
+                <div className="col-lg-12">
+                    <img src={this.props.imageURL} />
+                </div>
             </div>
         );
     }
 });
 
 var Holder = React.createClass({
+    getInitialState: function() {
+        return {
+            tagsChecked: ['bridge','people']
+        };
+    },
+    handleUserInput: function(checkedTagName, isChecked) {
+        var tagsChecked = this.state.tagsChecked;
+        var index = tagsChecked.indexOf(checkedTagName);
+        if (index > -1) {
+            tagsChecked.splice(index, 1);
+        } else {
+            tagsChecked.push(checkedTagName);
+        }
+        this.setState({
+            tagsChecked: tagsChecked 
+        });
+    },
     render: function() {
         return (
-            <div className="holder">
-                <SlideView imageURL={this.props.image.imageURL} />
-                <TagTable tagNames={this.props.image.tagNames} />
+            <div className="holder container-fluid">
+                <SlideView 
+                    imageURL={this.props.image.imageURL} 
+                />
+                <TagTable 
+                    tagNames={this.props.image.tagNames} 
+                    tagsChecked={this.state.tagsChecked}
+                    onUserInput={this.handleUserInput}
+                />
             </div>
         );
     }
 });
 
-
 image = {
     tagNames: [
-    'bridge',
-    'people',
-    'monument',
-    'architecture'
+        'bridge',
+        'people',
+        'monument',
+        'architecture'
     ],
     imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Taj_Mahal_in_March_2004.jpg/200px-Taj_Mahal_in_March_2004.jpg'
 };
